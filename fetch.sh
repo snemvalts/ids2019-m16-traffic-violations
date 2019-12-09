241d9bba57539ba033f030c5f26c2c530fe082d4
Sander Nemvalts <snemvalts@gmail.com>
2019-12-10 00:33:25 +0200

    booyaka

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+rm -rf data/
 echo "getting last 5 years worth"
 mkdir data
 cd data
 wget https://opendata.smit.ee/ppa/csv/liiklusjarelevalve_2.csv
+
+echo "getting map outline from maaamet"
+mkdir map
+cd map
+wget https://geoportaal.maaamet.ee/docs/haldus_asustus/maakond_shp.zip
+unzip maakond_shp.zip
+
